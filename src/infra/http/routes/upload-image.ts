0fb2fbee2eed465e72f046856b232cdebca995bf
Augusto Moscardo <augustomsaluz@gmail.com>
2025-01-26 13:02:36 -0300

    server routes

---
+++ src/infra/http/routes/upload-image.ts
@@ -0,0 +1,57 @@
+import { uploadImage } from '@/app/functions/upload-image'
+import { isRight, unwrapEither } from '@/infra/shared/either'
+import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+
+export const uploadImageRoute: FastifyPluginAsyncZod = async server => {
+  server.post(
+    '/uploads',
+    {
+      schema: {
+        summary: 'Upload an image',
+        tags: ['uploads'],
+        consumes: ['multipart/form-data'],
+        response: {
+          201: z.null().describe('Image uploaded'),
+          400: z.object({ message: z.string() }),
+        },
+      },
+    },
+    async (request, reply) => {
+      const uploadedFile = await request.file({
+        limits: {
+          fileSize: 1024 * 1024 * 2, // 2MB
+        },
+      })
+
+      if (!uploadedFile) {
+        return reply.status(400).send({ message: 'File is required.' })
+      }
+
+      const result = await uploadImage({
+        fileName: uploadedFile.filename,
+        contentType: uploadedFile.mimetype,
+        contentStream: uploadedFile.file,
+      })
+
+      if (uploadedFile.file.truncated) {
+        //
+        return reply.status(400).send({ message: 'File size limit reached.' })
+      }
+
+      //checa se o resultado é um sucesso
+      if (isRight(result)) {
+        console.log(unwrapEither(result))
+
+        return reply.status(201).send()
+      }
+
+      const error = unwrapEither(result) // Nesse trecho sabe-se que é um erro. A fn vai instanciar a classe de erro e retornar o erro.
+
+      switch (error.constructor.name) {
+        case 'InvalidFileFormat':
+          return reply.status(400).send({ message: error.message })
+      }
+    }
+  )
+}
